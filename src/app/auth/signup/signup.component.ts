0f801a56cf6e005aa77a2197d80b90f6ac1f76ce
thanhthu170699 <thanhthunguyen1706@gamil.com>
2020-11-30 23:19:28 +0700

    update signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {UserService} from '../../core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private fb:FormBuilder,
+      private userService:UserService,
+      private router: Router
+  
+  ) { }
 
   ngOnInit(): void {
   }
-
+  signUpForm = this.fb.group({
+    firstname:['',Validators.required],
+    lastname:['',Validators.required],
+    email:['',Validators.email],
+    password:['',Validators.minLength(6)],
+  })
+  onSubmitSignUp(): void {
+    const credentials = this.signUpForm.value;
+  this.userService.signup(credentials)
+  .subscribe(
+    
+    data => {
+      if(data){
+        this.router.navigate([''])
+      }
+    },
+    error => console.log(error)
+  );
+  alert('Register Succeeded');
+ }  
 }
