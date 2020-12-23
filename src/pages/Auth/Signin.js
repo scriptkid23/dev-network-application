d830429717400114d46df861f24438ac692e9776
nguyenhai1999 <45683031+nguyenhai1999@users.noreply.github.com>
2020-12-23 14:57:39 +0700

    intergrate-api-signin

--- src/pages/Auth/Signin.js
+++ src/pages/Auth/Signin.js
@@ -2,37 +2,51 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Spirity from '../../helper/hook'
 import { useForm } from "react-hook-form";
-import {ShowAlert,Backdrop} from '../../components/common/index';
+import { ShowAlert, Backdrop } from '../../components/common/index';
 
 export function Signin() {
     const { store, action } = Spirity();
     var authStore = store.authStore;
-    const { login, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const onSubmit = data => {
-    action.login(data)
+        action.login(data)
     };
 
     return (
         <div className="container d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
             {authStore.loading && <Backdrop show={authStore.loading ? true : false} />}
+
             <div className="row no-gutters text-center align-items-center justify-content-center min-vh-100">
                 <div className="col-12 col-md-6 col-lg-5 col-xl-4">
+                    <ShowAlert
+                        show={authStore.variant.length !== 0 ? true : false}
+                        variant={authStore.variant}
+                        notification={authStore.message}
+                    />
                     <h1 className="font-weight-bold">Sign in</h1>
                     <p className="text-dark mb-3">We are Different, We Make You Different.</p>
                     <form className="mb-3" >
                         <div className="form-group">
-                            <input
+                            <label for="email" className="sr-only">Email</label>
+                            <input formControlName="email"
                                 required
-                                type="email"
+                                name="email"
+                                type="text"
+                                ref={register({ required: true })}
                                 className="form-control form-control-md"
                                 id="email" placeholder="Enter your email" />
+
                         </div>
                         <div className="form-group">
-                            <input
-                                type="password"
+                            <label for="password" className="sr-only">Password</label>
+                            <input formControlName="password"
                                 required
+                                name="password"
+                                type="text"
+                                ref={register({ required: true })}
                                 className="form-control form-control-md"
                                 id="password" placeholder="Enter your password" />
+
                         </div>
                         <div className="form-group d-flex justify-content-end">
                             <Link className="font-size-sm" to="/auth/password/recovery">Reset password</Link>
@@ -41,15 +55,11 @@ export function Signin() {
                             className="btn btn-primary btn-lg btn-block text-uppercase font-weight-semibold"
                             type="submit"
                             onClick={handleSubmit(onSubmit)} >
-                    Sign in
+                            Sign in
                 </button>
                     </form>
                     <p>Don't have an account? <Link className="font-weight-semibold" to="/auth/register">Sign up</Link>.</p>
-                    <ShowAlert
-                        show={authStore.variant.length !== 0 ? true : false}
-                        variant={authStore.variant}
-                        notification = {authStore.message}
-                    />
+
                 </div>
             </div>
         </div>
